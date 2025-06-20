51b0cc009d7d46fee2cd21684fecdf53b03fa60c
ExoCode33 <emmanuel@supracode.ca>
2025-06-20 13:05:46 -0400

    Create bot.js

---
+++ config/bot.js
@@ -0,0 +1,16 @@
+module.exports = {
+    // Animation timing
+    timings: {
+        initialPull: 1500,
+        spinFrame: 300,
+        spinFrames: 8,
+        rarityReveal: 2000
+    },
+    
+    // Colors
+    colors: {
+        default: '#0099FF',
+        error: '#FF0000',
+        success: '#00FF00'
+    }
+};
