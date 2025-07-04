634394b3cfbe2c00acf96c303ec5b385f3f2e476
ExoCode33 <emmanuel@supracode.ca>
2025-07-04 18:20:01 -0400

    Update counter-system.js

--- src/data/counter-system.js
+++ src/data/counter-system.js
@@ -149,72 +149,175 @@ const COUNTER_MATRIX = {
     }
 };
 
-// DEVIL FRUIT ELEMENT MAPPING - COMPLETE WITH ALL FRUITS
+// DEVIL FRUIT ELEMENT MAPPING - Updated for your 150-fruit database
 const DEVIL_FRUIT_ELEMENTS = {
     // LOGIA FRUITS
-    'mera_mera_001': ELEMENT_TYPES.FIRE,           // Ace's Fire
-    'hie_hie_001': ELEMENT_TYPES.ICE,             // Aokiji's Ice  
-    'pika_pika_001': ELEMENT_TYPES.LIGHT,         // Kizaru's Light
-    'goro_goro_001': ELEMENT_TYPES.LIGHTNING,     // Enel's Lightning
-    'magu_magu_001': ELEMENT_TYPES.MAGMA,         // Akainu's Magma
-    'yami_yami_001': ELEMENT_TYPES.DARKNESS,      // Blackbeard's Darkness
-    'suna_suna_001': ELEMENT_TYPES.SAND,          // Crocodile's Sand
-    'gasu_gasu_001': ELEMENT_TYPES.GAS,           // Caesar's Gas
+    'fruit_002': ELEMENT_TYPES.FIRE,           // Mera Mera no Mi
+    'fruit_021': ELEMENT_TYPES.ICE,            // Hie Hie no Mi  
+    'fruit_004': ELEMENT_TYPES.LIGHT,          // Pika Pika no Mi
+    'fruit_020': ELEMENT_TYPES.LIGHTNING,      // Goro Goro no Mi
+    'fruit_003': ELEMENT_TYPES.MAGMA,          // Magu Magu no Mi
+    'fruit_005': ELEMENT_TYPES.DARKNESS,       // Yami Yami no Mi
+    'fruit_019': ELEMENT_TYPES.SAND,           // Suna Suna no Mi
+    'fruit_057': ELEMENT_TYPES.GAS,            // Gasu Gasu no Mi
+    'fruit_058': ELEMENT_TYPES.GAS,            // Numa Numa no Mi (swamp)
+    'fruit_059': ELEMENT_TYPES.ICE,            // Yuki Yuki no Mi (snow)
     
     // SPECIAL PARAMECIA
-    'gura_gura_001': ELEMENT_TYPES.VIBRATION,     // Whitebeard's Earthquake
-    'ope_ope_001': ELEMENT_TYPES.SPATIAL,         // Law's Room
-    'zushi_zushi_001': ELEMENT_TYPES.GRAVITY,     // Fujitora's Gravity
-    'soru_soru_001': ELEMENT_TYPES.SOUL,          // Big Mom's Soul
-    'doku_doku_001': ELEMENT_TYPES.POISON,        // Magellan's Poison
-    'nikyu_nikyu_001': ELEMENT_TYPES.SPATIAL,     // Kuma's Paw (space manipulation)
+    'fruit_006': ELEMENT_TYPES.VIBRATION,      // Gura Gura no Mi
+    'fruit_007': ELEMENT_TYPES.SPATIAL,        // Ope Ope no Mi
+    'fruit_027': ELEMENT_TYPES.SOUL,           // Soru Soru no Mi
+    'fruit_010': ELEMENT_TYPES.POISON,         // Doku Doku no Mi
+    'fruit_076': ELEMENT_TYPES.SPATIAL,        // Nikyu Nikyu no Mi (paw)
     
     // RUBBER/NIKA
-    'hito_hito_001': ELEMENT_TYPES.RUBBER,        // Luffy's Rubber/Nika
-    'gomu_gomu_001': ELEMENT_TYPES.RUBBER,        // Regular Rubber
+    'fruit_001': ELEMENT_TYPES.RUBBER,         // Gomu Gomu no Mi
+    'fruit_026': ELEMENT_TYPES.RUBBER,         // Hito Hito no Mi Model: Nika
     
     // ZOAN CATEGORIES
-    'hito_hito_002': ELEMENT_TYPES.ZOAN_BEAST,    // Chopper
-    'neko_neko_001': ELEMENT_TYPES.ZOAN_BEAST,    // Lucci's Leopard
-    'inu_inu_001': ELEMENT_TYPES.ZOAN_BEAST,      // Jabra's Wolf
-    'tori_tori_001': ELEMENT_TYPES.ZOAN_BEAST,    // Pell's Falcon
-    'ushi_ushi_001': ELEMENT_TYPES.ZOAN_BEAST,    // Dalton's Bison
-    
-    // ANCIENT ZOANS  
-    'ryu_ryu_001': ELEMENT_TYPES.ZOAN_ANCIENT,    // X-Drake's Allosaurus
-    'zou_zou_001': ELEMENT_TYPES.ZOAN_ANCIENT,    // Jack's Mammoth
-    'ryu_ryu_002': ELEMENT_TYPES.ZOAN_ANCIENT,    // Page One's Spinosaurus
+    'fruit_054': ELEMENT_TYPES.ZOAN_BEAST,     // Hito Hito no Mi (Chopper)
+    'fruit_045': ELEMENT_TYPES.ZOAN_BEAST,     // Neko Neko no Mi, Model: Leopard
+    'fruit_044': ELEMENT_TYPES.ZOAN_BEAST,     // Inu Inu no Mi, Model: Wolf
+    'fruit_047': ELEMENT_TYPES.ZOAN_BEAST,     // Tori Tori no Mi, Model: Falcon
+    'fruit_046': ELEMENT_TYPES.ZOAN_BEAST,     // Ushi Ushi no Mi, Model: Bison
+    'fruit_050': ELEMENT_TYPES.ZOAN_BEAST,     // Zou Zou no Mi
+    'fruit_051': ELEMENT_TYPES.ZOAN_BEAST,     // Uma Uma no Mi
+    'fruit_055': ELEMENT_TYPES.ZOAN_BEAST,     // Inu Inu no Mi, Model: Dachshund
+    'fruit_056': ELEMENT_TYPES.ZOAN_BEAST,     // Ushi Ushi no Mi, Model: Giraffe
+    'fruit_048': ELEMENT_TYPES.ZOAN_BEAST,     // Hebi Hebi no Mi, Model: Anaconda
+    'fruit_049': ELEMENT_TYPES.ZOAN_BEAST,     // Hebi Hebi no Mi, Model: King Cobra
+    'fruit_052': ELEMENT_TYPES.ZOAN_BEAST,     // Sara Sara no Mi, Model: Axolotl
+    'fruit_053': ELEMENT_TYPES.ZOAN_BEAST,     // Kumo Kumo no Mi
     
     // MYTHICAL ZOANS
-    'uo_uo_001': ELEMENT_TYPES.ZOAN_MYTHICAL,     // Kaido's Dragon
-    'tori_tori_002': ELEMENT_TYPES.ZOAN_MYTHICAL, // Marco's Phoenix
-    'inu_inu_003': ELEMENT_TYPES.ZOAN_MYTHICAL,   // Yamato's Wolf Deity
+    'fruit_025': ELEMENT_TYPES.ZOAN_MYTHICAL,  // Uo Uo no Mi, Model: Seiryu (Kaido)
+    'fruit_024': ELEMENT_TYPES.ZOAN_MYTHICAL,  // Tori Tori no Mi, Model: Phoenix
+    'fruit_061': ELEMENT_TYPES.ZOAN_MYTHICAL,  // Inu Inu no Mi, Model: Okuchi no Makami
+    'fruit_062': ELEMENT_TYPES.ZOAN_MYTHICAL,  // Hebi Hebi no Mi, Model: Yamata no Orochi
+    'fruit_060': ELEMENT_TYPES.ZOAN_MYTHICAL,  // Hito Hito no Mi, Model: Daibutsu
     
-    // PARAMECIA FRUITS - COMPLETE MAPPING
-    'kilo_kilo_001': ELEMENT_TYPES.GRAVITY,       // Miss Valentine's Weight (gravity-based)
-    'sube_sube_001': ELEMENT_TYPES.RUBBER,        // Alvida's slip (deflection like rubber)
-    'bomu_bomu_001': ELEMENT_TYPES.FIRE,          // Mr. 5's explosions (fire-based)
-    'hana_hana_001': ELEMENT_TYPES.ZOAN_BEAST,    // Robin's sprouting (biological)
-    'bara_bara_001': ELEMENT_TYPES.METAL,         // Buggy's splitting (sharp resistance)
-    'doru_doru_001': ELEMENT_TYPES.STONE,         // Mr.3's wax (hardening)
-    'buki_buki_001': ELEMENT_TYPES.METAL,         // Baby 5's weapons
-    'mero_mero_001': ELEMENT_TYPES.SOUL,          // Hancock's love (affects soul/mind)
-    'noro_noro_001': ELEMENT_TYPES.GRAVITY,       // Foxy's slow (time/space)
-    'awa_awa_001': ELEMENT_TYPES.POISON,          // Kalifa's soap (chemical)
-    'supa_supa_001': ELEMENT_TYPES.METAL,         // Mr. 1's blade body
-    'toge_toge_001': ELEMENT_TYPES.METAL,         // Miss Doublefinger's spikes
-    'ori_ori_001': ELEMENT_TYPES.METAL,           // Hina's cage/binding
-    'door_door_001': ELEMENT_TYPES.SPATIAL,       // Blueno's doors
-    'berry_berry_001': ELEMENT_TYPES.RUBBER,      // Very Good's sphere body
-    'shari_shari_001': ELEMENT_TYPES.METAL,       // Sharinguru's wheels
-    'yomi_yomi_001': ELEMENT_TYPES.SOUL,          // Brook's soul revival
-    'kage_kage_001': ELEMENT_TYPES.DARKNESS,      // Moria's shadows
-    'horo_horo_001': ELEMENT_TYPES.SOUL,          // Perona's ghosts
-    'suke_suke_001': ELEMENT_TYPES.LIGHT,         // Absalom's invisibility
-    'memo_memo_001': ELEMENT_TYPES.SOUL,          // Pudding's memory manipulation
-    'tama_tama_001': ELEMENT_TYPES.ZOAN_BEAST,    // Tamago's egg body
-    'bari_bari_001': ELEMENT_TYPES.SPATIAL,       // Bartolomeo's barriers
-    'mochi_mochi_001': ELEMENT_TYPES.RUBBER,      // Katakuri's mochi (flexible like rubber)
+    // PARAMECIA FRUITS - MAJOR ONES
+    'fruit_015': ELEMENT_TYPES.GRAVITY,        // Kilo Kilo no Mi (weight)
+    'fruit_014': ELEMENT_TYPES.RUBBER,         // Sube Sube no Mi (slip)
+    'fruit_012': ELEMENT_TYPES.FIRE,           // Bomu Bomu no Mi (explosions)
+    'fruit_016': ELEMENT_TYPES.ZOAN_BEAST,     // Hana Hana no Mi (biological)
+    'fruit_013': ELEMENT_TYPES.METAL,          // Bara Bara no Mi (splitting)
+    'fruit_017': ELEMENT_TYPES.STONE,          // Doru Doru no Mi (wax)
+    'fruit_075': ELEMENT_TYPES.SOUL,           // Mero Mero no Mi (love)
+    'fruit_008': ELEMENT_TYPES.METAL,          // Ito Ito no Mi (string)
+    'fruit_009': ELEMENT_TYPES.SOUL,           // Yomi Yomi no Mi (soul)
+    'fruit_011': ELEMENT_TYPES.SPATIAL,        // Bari Bari no Mi (barriers)
+    'fruit_022': ELEMENT_TYPES.POISON,         // Horu Horu no Mi (hormones)
+    'fruit_023': ELEMENT_TYPES.RUBBER,         // Mochi Mochi no Mi (flexible)
+    'fruit_028': ELEMENT_TYPES.POISON,         // Awa Awa no Mi (soap)
+    'fruit_030': ELEMENT_TYPES.SOUL,           // Memo Memo no Mi (memory)
+    'fruit_031': ELEMENT_TYPES.SOUL,           // Hobi Hobi no Mi (toy transformation)
+    'fruit_032': ELEMENT_TYPES.SOUL,           // Wara Wara no Mi (voodoo)
+    'fruit_034': ELEMENT_TYPES.GRAVITY,        // Ton Ton no Mi (weight)
+    'fruit_035': ELEMENT_TYPES.STONE,          // Ishi Ishi no Mi (stone)
+    'fruit_063': ELEMENT_TYPES.SPATIAL,        // Nagi Nagi no Mi (sound/silence)
+    'fruit_064': ELEMENT_TYPES.SPATIAL,        // Mato Mato no Mi (target)
+    'fruit_067': ELEMENT_TYPES.SPATIAL,        // Shiro Shiro no Mi (castle)
+    'fruit_068': ELEMENT_TYPES.METAL,          // Ori Ori no Mi (cage)
+    'fruit_072': ELEMENT_TYPES.GRAVITY,        // Fuwa Fuwa no Mi (float)
+    'fruit_074': ELEMENT_TYPES.LIGHT,          // Suke Suke no Mi (invisibility)
+    'fruit_078': ELEMENT_TYPES.DARKNESS,       // Kage Kage no Mi (shadows)
+    'fruit_080': ELEMENT_TYPES.METAL,          // Buki Buki no Mi (weapons)
+    'fruit_018': ELEMENT_TYPES.METAL,          // Baku Baku no Mi (eating/metal)
+    'fruit_029': ELEMENT_TYPES.POISON,         // Beta Beta no Mi (sticky)
+    'fruit_033': ELEMENT_TYPES.VIBRATION,      // Goe Goe no Mi (voice/sound waves)
+    'fruit_036': ELEMENT_TYPES.SPATIAL,        // Sui Sui no Mi (swimming through solids)
+    'fruit_037': ELEMENT_TYPES.GRAVITY,        // Guru Guru no Mi (spinning)
+    'fruit_038': ELEMENT_TYPES.SOUL,           // Ato Ato no Mi (art/transformation)
+    'fruit_039': ELEMENT_TYPES.SPATIAL,        // Buku Buku no Mi (book world)
+    'fruit_040': ELEMENT_TYPES.POISON,         // Kuku Kuku no Mi (cooking/food)
+    'fruit_041': ELEMENT_TYPES.METAL,          // Nui Nui no Mi (sewing/binding)
+    'fruit_042': ELEMENT_TYPES.RUBBER,         // Jake Jake no Mi (jacket/flexibility)
+    'fruit_043': ELEMENT_TYPES.METAL,          // Kama Kama no Mi (sickle/cutting)
+    'fruit_065': ELEMENT_TYPES.RUBBER,         // Beri Beri no Mi (sphere/round)
+    'fruit_066': ELEMENT_TYPES.GRAVITY,        // Mini Mini no Mi (size manipulation)
+    'fruit_069': ELEMENT_TYPES.SPATIAL,        // Ami Ami no Mi (net/capture)
+    'fruit_070': ELEMENT_TYPES.SOUL,           // Yomi Yomi no Mi (soul revival)
+    'fruit_071': ELEMENT_TYPES.METAL,          // Gasha Gasha no Mi (combining/mechanical)
+    'fruit_073': ELEMENT_TYPES.METAL,          // Kama Kama no Mi (duplicate - cutting)
+    'fruit_077': ELEMENT_TYPES.SOUL,           // Hiso Hiso no Mi (whisper/communication)
+    'fruit_079': ELEMENT_TYPES.SOUL,           // Yomi Yomi no Mi (duplicate - soul)
+    'fruit_081': ELEMENT_TYPES.POISON,         // Iro Iro no Mi (color/paint)
+    'fruit_082': ELEMENT_TYPES.SPATIAL,        // Nuke Nuke no Mi (passing through)
+    'fruit_083': ELEMENT_TYPES.RUBBER,         // Lika Lika no Mi (licking/flexible)
+    'fruit_084': ELEMENT_TYPES.POISON,         // Woshu Woshu no Mi (washing/cleaning)
+    'fruit_085': ELEMENT_TYPES.VIBRATION,      // Goe Goe no Mi (duplicate - voice)
+    'fruit_086': ELEMENT_TYPES.SOUL,           // Horo Horo no Mi (ghosts/negative spirits)
+    'fruit_087': ELEMENT_TYPES.SOUL,           // Tama Tama no Mi (egg/rebirth)
+    
+    // SMILE FRUITS (all as basic zoan)
+    'fruit_088': ELEMENT_TYPES.ZOAN_BEAST,     // Sheep SMILE
+    'fruit_089': ELEMENT_TYPES.ZOAN_BEAST,     // Horse SMILE
+    'fruit_090': ELEMENT_TYPES.ZOAN_BEAST,     // Lion SMILE
+    'fruit_091': ELEMENT_TYPES.ZOAN_BEAST,     // Bat SMILE
+    'fruit_092': ELEMENT_TYPES.ZOAN_BEAST,     // Gazelle SMILE
+    'fruit_093': ELEMENT_TYPES.ZOAN_BEAST,     // Elephant SMILE
+    'fruit_094': ELEMENT_TYPES.ZOAN_BEAST,     // Scorpion SMILE
+    'fruit_095': ELEMENT_TYPES.ZOAN_BEAST,     // Giraffe SMILE
+    'fruit_096': ELEMENT_TYPES.ZOAN_BEAST,     // Alpaca SMILE
+    'fruit_097': ELEMENT_TYPES.ZOAN_BEAST,     // Hedgehog SMILE
+    'fruit_098': ELEMENT_TYPES.ZOAN_BEAST,     // Hippo SMILE
+    'fruit_099': ELEMENT_TYPES.ZOAN_BEAST,     // Snake SMILE
+    'fruit_100': ELEMENT_TYPES.ZOAN_BEAST,     // Gorilla SMILE
+    'fruit_101': ELEMENT_TYPES.ZOAN_BEAST,     // Mouse SMILE
+    'fruit_102': ELEMENT_TYPES.ZOAN_BEAST,     // Elephant Trunk SMILE
+    
+    // Generic SMILE fruits (103-137) - all ZOAN_BEAST
+    'fruit_103': ELEMENT_TYPES.ZOAN_BEAST,
+    'fruit_104': ELEMENT_TYPES.ZOAN_BEAST,
+    'fruit_105': ELEMENT_TYPES.ZOAN_BEAST,
+    'fruit_106': ELEMENT_TYPES.ZOAN_BEAST,
+    'fruit_107': ELEMENT_TYPES.ZOAN_BEAST,
+    'fruit_108': ELEMENT_TYPES.ZOAN_BEAST,
+    'fruit_109': ELEMENT_TYPES.ZOAN_BEAST,
+    'fruit_110': ELEMENT_TYPES.ZOAN_BEAST,
+    'fruit_111': ELEMENT_TYPES.ZOAN_BEAST,
+    'fruit_112': ELEMENT_TYPES.ZOAN_BEAST,
+    'fruit_113': ELEMENT_TYPES.ZOAN_BEAST,
+    'fruit_114': ELEMENT_TYPES.ZOAN_BEAST,
+    'fruit_115': ELEMENT_TYPES.ZOAN_BEAST,
+    'fruit_116': ELEMENT_TYPES.ZOAN_BEAST,
+    'fruit_117': ELEMENT_TYPES.ZOAN_BEAST,
+    'fruit_118': ELEMENT_TYPES.ZOAN_BEAST,
+    'fruit_119': ELEMENT_TYPES.ZOAN_BEAST,
+    'fruit_120': ELEMENT_TYPES.ZOAN_BEAST,
+    'fruit_121': ELEMENT_TYPES.ZOAN_BEAST,
+    'fruit_122': ELEMENT_TYPES.ZOAN_BEAST,
+    'fruit_123': ELEMENT_TYPES.ZOAN_BEAST,
+    'fruit_124': ELEMENT_TYPES.ZOAN_BEAST,
+    'fruit_125': ELEMENT_TYPES.ZOAN_BEAST,
+    'fruit_126': ELEMENT_TYPES.ZOAN_BEAST,
+    'fruit_127': ELEMENT_TYPES.ZOAN_BEAST,
+    'fruit_128': ELEMENT_TYPES.ZOAN_BEAST,
+    'fruit_129': ELEMENT_TYPES.ZOAN_BEAST,
+    'fruit_130': ELEMENT_TYPES.ZOAN_BEAST,
+    'fruit_131': ELEMENT_TYPES.ZOAN_BEAST,
+    'fruit_132': ELEMENT_TYPES.ZOAN_BEAST,
+    'fruit_133': ELEMENT_TYPES.ZOAN_BEAST,
+    'fruit_134': ELEMENT_TYPES.ZOAN_BEAST,
+    'fruit_135': ELEMENT_TYPES.ZOAN_BEAST,
+    'fruit_136': ELEMENT_TYPES.ZOAN_BEAST,
+    'fruit_137': ELEMENT_TYPES.ZOAN_BEAST,
+    
+    // Special anime/movie fruits
+    'fruit_138': ELEMENT_TYPES.FIRE,           // Atsu Atsu no Mi (heat)
+    'fruit_139': ELEMENT_TYPES.SPATIAL,        // Ami Ami no Mi (net)
+    'fruit_140': ELEMENT_TYPES.STONE,          // Kachi Kachi no Mi (hardness)
+    'fruit_141': ELEMENT_TYPES.VIBRATION,      // Goe Goe no Mi (voice)
+    'fruit_142': ELEMENT_TYPES.SOUL,           // Hiso Hiso no Mi (whisper)
+    'fruit_143': ELEMENT_TYPES.POISON,         // Mitsu Mitsu no Mi (honey)
+    'fruit_144': ELEMENT_TYPES.SOUL,           // Nemu Nemu no Mi (sleep)
+    'fruit_145': ELEMENT_TYPES.SPATIAL,        // Sui Sui no Mi (swim)
+    'fruit_146': ELEMENT_TYPES.GRAVITY,        // Fuwa Fuwa no Mi (float)
+    'fruit_147': ELEMENT_TYPES.METAL,          // Gasha Gasha no Mi (combine)
+    'fruit_148': ELEMENT_TYPES.RUBBER,         // Mochi Mochi no Mi (special)
+    'fruit_149': ELEMENT_TYPES.SPATIAL,        // Kama Kama no Mi (sickle)
+    'fruit_150': ELEMENT_TYPES.RUBBER,         // Toro Toro no Mi (liquid)
 };
 
 // COMBAT EFFECTIVENESS CALCULATOR
